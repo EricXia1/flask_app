4a68c8b69680091944332772edde5475646e2847
Eric Xia <ercx1023@gmail.com>
2024-04-07 21:33:58 -0400

    bingus

--- app.py
+++ app.py
@@ -1,11 +1,6 @@
 from flask import Flask, request, render_template
 app = Flask(__name__)
 
-ButtonPressed = 0
-@app.route('/', methods=["GET", "POST"])
+@app.route('/')
 def button():
-    global ButtonPressed
-    if request.method == "POST":
-        ButtonPressed += 1
-        return render_template("front_end.html", ButtonPressed = ButtonPressed)
-    return render_template("front_end.html", ButtonPressed = ButtonPressed)
+    return render_template("front_end.html", ButtonPressed = "hi")
